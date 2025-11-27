9680158272ca7fda83c13437489470ec919c6e2b
CodeAssasinKing <bayramowramazan85@gmail.com>
2025-11-27 10:39:34 +0500

    project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+
+function App() {
+
+    return (
+        <div className="container">
+            <h1>Website is still under development</h1>
+            <p>We’re building something new. Please check back soon.</p>
+        </div>
+    )
+}
+
+
+export default App;
